55bf9c964d0ede06c54eecdba32b0ebf79c2a008
Inokcode <regilearn@gmail.com>
2021-08-04 15:35:28 +0530

    Addon added

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,17 @@
 import React, { HTMLAttributes, ReactNode } from 'react'
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
+    /**
+     * Provide a text for the button
+     */
     children: ReactNode;
+    /**
+     * Which variant look would you like use
+     */
     variant: 'primary' | 'secondary';
 }
 
+/**This is a special button!!! */
 export const Button = ({ children, variant, ...props }: Props) => {
     return (
         <button {...props} style={{ backgroundColor: variant === 'primary' ? 'blue' : 'gray', color: 'white' }}>
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react";
 import { Button, Props } from "../src/Button";
+import { action } from "@storybook/addon-actions";
 
 const meta: Meta = {
     title: 'Button',
     component: Button,
     argTypes: {
+        onClick: { action: 'clicked' },
         children: {
             defaultValue: 'Default Text'
         }
@@ -26,7 +28,8 @@ export const Secondary = Template.bind({});
 
 Secondary.args = {
     variant: 'secondary',
-    children: 'I am secondary'
+    children: 'I am secondary',
+    onClick: action('secondary click')
 }
 
 
